f9210464fd4a2184e580cf555370705298f61e85
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-08-24 17:30:23 +0300

    adding test Relates #22

--- server/__test__/test.js
+++ server/__test__/test.js
@@ -1,4 +1,18 @@
+const { beforeEach, afterAll, test, expect } = require('@jest/globals');
+const dbBuild = require('../database/bulid');
+const connection = require('../database/config/connection');
+
+const {getMealsFromDB} = require('../database/queries/meals');
+
+beforeEach(() => dbBuild);
+afterAll(() => connection.end());
+
 
 test('dummy', () => {
     expect(1).toBe(1)
 })
+
+test('Test getMeals', () => getMealsFromDB()
+  .then((data) => {
+    expect(data.rows.length).toBe(4);
+  }));
