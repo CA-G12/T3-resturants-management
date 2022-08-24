0b2e62a66da4ad1cc8e5279f5cb69471be648776
Ahmed Abu Sharar <absam9945@gmail.com>
2022-08-24 17:07:35 +0300

    Fetch chefs for the form and handle postMeals queires relates #20

--- server/controllers/meals/postMeal.js
+++ server/controllers/meals/postMeal.js
@@ -1,8 +1,9 @@
 const { addMeal } = require('../../database/queries/index');
 
 const postMeal = (req, res) => {
-  console.log('hello');
-  addMeal().then((data) => console.log(data.rows)).catch((erorr) => console.log(erorr));
+  console.log(req.body);
+  const {name, image, price, numOfPeople, chef_id} = req.body;
+  addMeal(name, image, price, Number(numOfPeople), chef_id).then((data) =>res.redirect('/')).catch((erorr) => console.log(erorr));
 };
 
 module.exports = postMeal;
