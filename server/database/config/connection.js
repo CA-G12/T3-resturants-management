f18e32362b344961b62f7943347b6291fd882119
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-08-24 17:20:18 +0300

    fix the key of heroku db Relates #22

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -2,14 +2,14 @@
 const { Pool } = require('pg');
 require('env2')('.env');
 
-const {DB_TEST_URL , DB_PRODUCTION_URL , NODE_ENV} = process.env;
+const {DB_TEST_URL , DATABASE_URL , NODE_ENV} = process.env;
 let URL = '';
 if(NODE_ENV == 'test'){
     console.log("test url is done .");
     URL = DB_TEST_URL;
 }else if(NODE_ENV == 'production'){
     console.log("PRODUCTION url is done .");
-    URL = DB_PRODUCTION_URL;
+    URL = DATABASE_URL;
 }else{
     throw new Error("URL_error");
 }
