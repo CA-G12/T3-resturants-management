e0683118b211c64c303de2203e4b60fbc26f0bae
Zayan-Alaraishy <59280598+Zayan-Alaraishy@users.noreply.github.com>
2022-08-27 00:55:32 +0300

    Delete resturants.js

--- server/routes/resturants.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const {
-  getResturants,postResturant, deleteResturant
-} = require('../controllers');
-
-router.route('/').get(getResturants).post(postResturant);
-router.post('/delete-resturant/:id',deleteResturant );
-
-module.exports = router;
